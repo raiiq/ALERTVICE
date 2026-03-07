1d5931e5c9aa5050e2dd279d42c84775947de09b
Baraa Basim <outofrai@gmail.com>
2026-03-07 22:14:15 +0300

    Fix next dev failure by removing conflicting webpack config on Next 16

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Prevent globe.gl and three from being bundled on the server
-      config.externals = [
-        ...(config.externals || []),
-        "globe.gl",
-        "three",
-      ];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
